d18e7fdc24cab0cd9fc7f699201273aab57a8f8d
ItsHachiMane <spillednothing@gmail.com>
2026-04-22 17:42:05 -0500

    delted repeated section

--- auction_site/settings.py
+++ auction_site/settings.py
@@ -61,5 +61,6 @@ STATIC_URL = "static/"
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
